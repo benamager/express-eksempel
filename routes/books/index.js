530addecfd7f5717ff758fc0fdf3352471410a8b
benamager <benaprivat@gmail.com>
2022-12-07 13:08:53 +0100

    Add image upload

--- routes/books/index.js
+++ routes/books/index.js
@@ -1,15 +1,32 @@
+import multer from "multer"
 import authorization from "../../middlewares/authorization.js"
 import getHandler from "./getHandler.js"
 import postHandler from "./postHandler.js"
 import putHandler from "./putHandler.js"
 import deleteHandler from "./deleteHandler.js"
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.jpg')
+  },
+})
+
+const upload = multer({ storage: storage, limits: { fileSize: 10000 } })
+
+
+
 const books = (app) => {
   app.route('/books/:id?') // ? means optional parameter
+    //.all(authorization)
     .get(getHandler)
-    .post(authorization, postHandler)
-    .put(authorization, putHandler)
-    .delete(authorization, deleteHandler)
+    .all(authorization)
+    .post(upload.single('img'), postHandler)
+    .put(putHandler)
+    .delete(deleteHandler)
 }
 
 export default books
--- routes/books/postHandler.js
+++ routes/books/postHandler.js
@@ -1,10 +1,24 @@
-const postHandler = (req, res) => {
-  const { title, author } = req.body
-  if (!title || !author) {
-    res.status(422).json({ message: "Title and author is required." })
+import sharp from "sharp"
+
+const postHandler = async (req, res) => {
+  const { title, author } = req.body || null
+  const imgFile = req.file || null
+  const imgPath = imgFile ? imgFile.path.replace("public/", "localhost:3000/") : null
+
+  if (!title || !author || !imgFile) {
+    res.status(422).json({ message: "Title, author and image is required." })
     res.end()
+    return
   }
-  res.send(`Add book with title: ${title} and author: ${author}`).end()
+
+  res.status(200).json({ message: "Book succesfully created", object: { title, author, imgLocation: imgPath } })
+
+  // const { title, author } = req.body
+  // if (!title || !author) {
+  //   res.status(422).json({ message: "Title and author is required." })
+  //   res.end()
+  // }
+  // res.send(`Add book with title: ${title} and author: ${author}`).end()
 }
 
 export default postHandler;
