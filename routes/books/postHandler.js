480682ec583903088f4311d6e708815d1986363d
benamager <benaprivat@gmail.com>
2022-12-08 14:13:27 +0100

    Add multiple and shorten code

--- routes/books/postHandler.js
+++ routes/books/postHandler.js
@@ -1,32 +1,38 @@
 import useDB from "../../functions/useDB.js"
 
 const postHandler = async (req, res) => {
-  const { title, description, price } = req.body || null
-  const imgFile = req.file || null
-  const imgPath = imgFile ? imgFile.path.replace("public/", "/") : null
+  const requiredKeys = ["title", "description", "price"]; // required keys for req.body
 
-  if (!title || !description, !price || !imgFile) {
-    res.status(422).json({ message: "Title, description, price and image is required." })
-    res.end()
+  const isArray = Array.isArray(req.body)
+  const { collection: booksCollection, client } = await useDB("BooksExpress", "books") // connect to db
+
+  const singleBook = [Object.assign({}, req.body)] // assign req.body to remove multipart form validation
+  const data = isArray ? req.body : singleBook // set data to raw data or converted data in array format
+
+  // check if all required keys are present
+  const isValid = data.every((obj) => {
+    const hasRequiredKeys = requiredKeys.every((key) => obj.hasOwnProperty(key));
+    return hasRequiredKeys;
+  });
+
+  // send error if not valid
+  if (isValid === false) {
+    res.status(422).json({ message: "Title, description, price and image is required." }).end()
     return
   }
 
-  // connect to db
-  const { collection: booksCollection, client } = await useDB("BooksExpress", "books")
-
   try {
-    // insert book
-    const result = await booksCollection.insertOne({ title, description, price });
-    // return 201 and book
-    res.status(201).json({
-      message: "Book created successfully.", book: {
-        _id: result.insertedId,
-        title,
-        description,
-        price,
-      }
-    })
+    // single book, insertOne
+    if (isArray === false) {
+      const result = await booksCollection.insertOne(data[0]);
+      res.status(201).json({ message: "Succesfully created book", book: result }).end()
+      return
+    }
+    // multiple books, insertMany
+    const result = await booksCollection.insertMany(data);
+    res.status(201).json({ message: "Succesfully created books", books: result }).end()
   } catch (error) {
+    // send error if something went wrong
     res.status(500).json({ message: "Something went wrong.", error: error })
   }
 
